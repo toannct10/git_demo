52f9a958f9ba5608b7377b0cef72852055b65674
toannct10 <ngcongtoan10@gmail.com>
2026-01-14 14:53:39 +0700

    dev:temp console changes

--- devops-youtube-course-2025/server/server.js
+++ devops-youtube-course-2025/server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('Changed again 123 - Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
